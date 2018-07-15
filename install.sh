5ebaab8959028075d44a081553d712d9ac5a863f
GOSSIP <40517663+GOSSIP-DEV@users.noreply.github.com>
2018-07-15 18:06:58 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install -y apache2 libapache2-mod-php php php-curl unzip
+apt-get install apache2 libapache2-mod-php php php-curl unzip -y
 service apache2 restart
 ufw allow http
 cd /var/www/html/
