117e6b9487bbcd77de63c1fc8ddec304a56e7d05
GOSSIP <40517663+GOSSIP-DEV@users.noreply.github.com>
2018-07-15 16:59:32 +0200

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $rpc_password = "your_long_gossip_rpc_password";
 $rpc_url = "localhost";
 $rpc_port = "22122";
 $refresh_delay = "10"; # refresh data every x seconds
-$date_format = 'd/m/Y H:i:s'; # How to format dates
+$date_format = 'd.m.Y H:i:s'; # How to format dates
 $gossip_explorer = "80.211.186.19";
 #### MORE MASTERNODES ADDRESSES ###
 $custom_mnlist = array("", "", "");
