fe6cf5b4f82a6567ba64843d4e331f655365b572
GOSSIP <40517663+GOSSIP-DEV@users.noreply.github.com>
2018-07-15 16:41:18 +0200

    Update config.php

--- config.php
+++ config.php
@@ -3,11 +3,11 @@
 $rpc_user = "gossiprpc";
 $rpc_password = "your_long_gossip_rpc_password";
 ########## (OPTIONS) #################################
-$rpc_url = "localhost"; # Default : localhost
-$rpc_port = "22123"; # Default : 41799
+$rpc_url = "localhost";
+$rpc_port = "22122";
 $refresh_delay = "10"; # refresh data every x seconds
 $date_format = 'd/m/Y H:i:s'; # How to format dates
-$gossip_explorer = "http://80.211.186.19"; # Optional
+$gossip_explorer = "80.211.186.19";
 #### MORE MASTERNODES ADDRESSES ###
 $custom_mnlist = array("", "", "");
 ?>
