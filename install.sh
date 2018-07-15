1e9dede5d1d34b9341422815bf15f03b66c1f9eb
GOSSIP <40517663+GOSSIP-DEV@users.noreply.github.com>
2018-07-15 18:11:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-apt-get install apache2 libapache2-mod-php php php-curl unzip -y
+echo -e "Install needed tools"
+apt-get update >/dev/null 2>&1
+apt-get install -y apache2 libapache2-mod-php php php-curl unzip
 service apache2 restart
 ufw allow http
 cd /var/www/html/
 git clone https://github.com/GOSSIP-DEV/GOSSIP-Monitor .
 rm index.html
 rm /root/install.sh
+echo -e "Finished. Please edit the configuration file under /var/www/html/config.php"
 exit 0
