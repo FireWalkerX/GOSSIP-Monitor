0c89f6ddc9132cc44bb01a10136221e1f4c365a3
GOSSIP <40517663+GOSSIP-DEV@users.noreply.github.com>
2018-07-15 18:04:08 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-apt-get install apache2 libapache2-mod-php php php-curl unzip
+apt-get install -y apache2 libapache2-mod-php php php-curl unzip
 service apache2 restart
 ufw allow http
 cd /var/www/html/
 git clone https://github.com/GOSSIP-DEV/GOSSIP-Monitor .
 rm index.html
+exit 0
