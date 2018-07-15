d4896105201111e08f59b4da395cb9ebc60e1602
GOSSIP <40517663+GOSSIP-DEV@users.noreply.github.com>
2018-07-15 15:43:07 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+########## CHANGE THIS ##################################
+$rpc_user = "gossiprpc";
+$rpc_password = "your_long_gossip_rpc_password";
+########## (OPTIONS) #################################
+$rpc_url = "localhost"; # Default : localhost
+$rpc_port = "41799"; # Default : 41799
+$refresh_delay = "10"; # refresh data every x seconds
+$date_format = 'm/d/Y H:i:s'; # How to format dates
+$gossip_explorer = "gossip.donkeypool.com"; # Optional
+#### MORE MASTERNODES ADDRESSES ###
+$custom_mnlist = array("", "", "");
+?>
