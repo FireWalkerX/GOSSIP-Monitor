15555bb4f4d7d3ca49f3e57350495a25e2439575
GOSSIP <40517663+GOSSIP-DEV@users.noreply.github.com>
2018-07-15 18:55:44 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,6 @@ rm index.html >/dev/null 2>&1
 rm install.sh >/dev/null 2>&1
 rm -r .git >/dev/null 2>&1
 rm LICENSE >/dev/null 2>&1
-rm README.md >/dev/null 2>&1
 rm _config.yml >/dev/null 2>&1
 rm /root/install.s* >/dev/null 2>&1
 echo -e "Finished. Please edit the configuration file under /var/www/html/config.php"
